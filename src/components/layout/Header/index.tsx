b40769abdfb11a39ec2b6371fdae0cd99e2fa791
Suhyxn <momentomori21489@gmail.com>
2022-09-17 16:19:08 +0900

    💇style : Add header logo image

---
+++ src/components/layout/Header/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <>
+      <>Header</>
+      <img src="../../../../public/images/Logo.png" alt="Logo" />
+    </>
+  )
+}
+
+export default Header
