24eb5d449f5fc8aad8438923737d37839e16b530
ShinJaeIl01 <ekvm555@naver.com>
2022-09-18 23:27:53 +0900

    #4 ✨feat: Create Link Button and Navbar

--- src/styles/UserNavbarLayout.tsx
+++ src/styles/UserNavbarLayout.tsx
@@ -1,20 +1,57 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import projectIcon from '/historyProject-icon.png';
+import videoIcon from '/historyVideo-icon.png';
+import myInfo from '/myInfo-icon.png';
 
 function UserNavbarLayout() {
   return (
-    <div>
+    <Container>
       {/* 히스토리링크 */}
-      <div>
-        <div>안녕</div>
-        <div></div>
-      </div>
+      <HistoryLinkBox>
+        <LinkButton to="/" marginbottom="1.4rem">
+          <Image src={projectIcon} alt="프로젝트히스토리아이콘" />
+        </LinkButton>
+        <LinkButton to="/">
+          <Image src={videoIcon} alt="영상히스토리아이콘" />
+        </LinkButton>
+      </HistoryLinkBox>
       {/* 내계정 */}
       <div>
-        <div></div>
+        <LinkButton to="/">
+          <Image src={myInfo} alt="사용자계정정보아이콘" />
+        </LinkButton>
       </div>
-    </div>
+    </Container>
   );
 }
+
+interface LinkButtonProps {
+  marginbottom?: string;
+}
+
+const Container = styled.div`
+  background-color: #e8ecf1;
+  width: 5rem;
+  height: 62.5rem;
+  position: fixed;
+  padding: 0 1.5rem;
+`;
+const HistoryLinkBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 2.6rem 0 50.1rem 0;
+`;
+const LinkButton =
+  styled(Link) <
+  LinkButtonProps >
+  `
+  margin-bottom: ${({ marginbottom }) => marginbottom};
+`;
+const Image = styled.img`
+  width: 2rem;
+`;
 
 export default UserNavbarLayout;
