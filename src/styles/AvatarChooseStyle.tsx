d043dcc86f8689710548fd6d622fa0c543e513d8
Kimchaewook <allurings2@naver.com>
2022-09-20 22:59:09 +0900

    ✨ feat: Create Styles Foloder and AvatarChooseStyle.tsx

---
+++ src/styles/AvatarChooseStyle.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+
+function AvatarChooseStyle() {
+
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default AvatarChooseStyle;
