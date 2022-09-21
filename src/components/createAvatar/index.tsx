6fdd28ba9cdfe4f6010e1b84da4a3c20fb87ef44
Kimchaewook <allurings2@naver.com>
2022-09-21 23:32:04 +0900

    ✨ feat: Add Avatar component

--- src/components/createAvatar/index.tsx
+++ src/components/createAvatar/index.tsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+import ChooseAvatar from './avatar/chooseAvatar';
+import Avatar from './avatar/index'
 
 function index() {
   return (
-    <div>index</div>
+    <>
+      <Avatar />
+    </>
   )
 }
 
