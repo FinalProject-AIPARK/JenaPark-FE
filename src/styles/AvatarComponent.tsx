81b33ddb737616337350c48ac96df49cfd7a1433
Kimchaewook <allurings2@naver.com>
2022-09-21 22:50:46 +0900

    ✨ feat : Create AvatarComponent.tsx

---
+++ src/styles/AvatarComponent.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+import AvatarChooseStyle from './AvatarChooseStyle';
+import AvatarOptionStyle from './AvatarOptionStyle';
+
+function AvatarComponent() {
+  return (
+    <>
+    </>
+  )
+}
+
+
+export default AvatarComponent
