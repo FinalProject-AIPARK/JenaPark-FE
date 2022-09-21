5d800760d8c147ba87a4205e3f0453f0785fc7d7
Kimchaewook <allurings2@naver.com>
2022-09-21 22:54:40 +0900

    ✨ feat : Create Components/avatar/index.tsx

---
+++ src/components/createAvatar/avatar/index.tsx
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+import ChooseAvatar from './chooseAvatar/index'
+import AvatarOption from './avatarOption/index'
+
+function index() {
+
+  return (
+    <>
+      <ChooseAvatar/>
+    </>
+  )
+}
+
+export default index
