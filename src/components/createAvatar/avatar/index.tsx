9df8fddcae712832e7f8d63cda253f68c7c7a5c7
Kimchaewook <allurings2@naver.com>
2022-09-23 13:51:30 +0900

    ✨ feat: Add Toggle Switch and Component Connect

--- src/components/createAvatar/avatar/index.tsx
+++ src/components/createAvatar/avatar/index.tsx
@@ -1,12 +1,20 @@
-import React, {useState} from 'react';
-import ChooseAvatar from './chooseAvatar/index'
-import AvatarOption from './avatarOption/index'
+import { useState } from 'react';
+import AvatarComponent from '../../../styles/AvatarComponent';
 
 function index() {
 
+  const [toggle, setToggle] = useState(true);
+
+  function toggleButton() {
+    setToggle(!toggle);
+  }
+
   return (
     <>
-      <ChooseAvatar/>
+      <AvatarComponent 
+        toggle={toggle}
+        setToggle={toggleButton}
+      />
     </>
   )
 }
