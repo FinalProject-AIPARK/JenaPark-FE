00a8db3783a8456adbaa215a2b107ceb8c28b680
Kim Mingu <MINGU757377@gmail.com>
2022-09-23 18:19:25 +0900

    #24 💇style: Setting global fonts

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,25 @@
-import { BrowserRouter } from 'react-router-dom'
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-import { Provider } from 'react-redux'
-import { store } from './store/store'
-import { createGlobalStyle } from 'styled-components'
-import reset from 'styled-reset'	
+import { BrowserRouter } from 'react-router-dom';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
 
 const GlobalStyle = createGlobalStyle`
 ${reset}
-`
+body {
+font-family: 'Noto Sans KR', 'Roboto', sans-serif;
+}
+`;
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <BrowserRouter>
-  <Provider store={store}>
-    <GlobalStyle />
-    <App />
-  </Provider>
-  </BrowserRouter>
-)
+    <Provider store={store}>
+      <GlobalStyle />
+      <App />
+    </Provider>
+  </BrowserRouter>,
+);
