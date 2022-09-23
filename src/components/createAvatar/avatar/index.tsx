2486c75164482bb5da1b4960ebcdef46c3a8b8de
Kimchaewook <allurings2@naver.com>
2022-09-23 15:06:09 +0900

    ✨ feat: Add Toggle and AvatarComponent

--- src/components/createAvatar/avatar/index.tsx
+++ src/components/createAvatar/avatar/index.tsx
@@ -1,12 +1,22 @@
 import React, {useState} from 'react';
-import ChooseAvatar from './chooseAvatar/index'
-import AvatarOption from './avatarOption/index'
+import ChooseAvatar from './chooseAvatar/index';
+import AvatarOption from './avatarOption/index';
+import AvatarComponent from '../../../styles/AvatarComponent';
 
 function index() {
 
+  const [toggle, setToggle] = useState(true);
+
+  function toggleButton() {
+    setToggle(!toggle);
+  }
+
   return (
     <>
-      <ChooseAvatar/>
+      <AvatarComponent 
+        toggle={toggle}
+        setToggle={toggleButton}
+      />
     </>
   )
 }
