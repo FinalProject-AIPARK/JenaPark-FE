1d97363025d877cb2799ccdae12ae231cd9b5224
Kim Mingu <MINGU757377@gmail.com>
2022-09-25 15:33:55 +0900

    #1 #13 ♻️ refactor: SignIn & SignUp Refactoring

--- src/components/signIn/SignInForm.tsx
+++ src/components/signIn/SignInForm.tsx
@@ -1,138 +1,48 @@
-import axios from 'axios';
-import { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useSignInMutation } from '@/api/useApi';
+import React from 'react';
+import { useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
-import { useSignInMutation } from '../../api/useApi';
-import { saveName } from '../../store/SignIn/signInSlice';
+import * as S from './style';
 
-interface formValuesType {
+interface SignInFormInputs {
   email: string;
   password: string;
 }
 
-const SignInForm = () => {
-  const initialValue = {
-    email: '',
-    password: '',
-  };
-  const [formValues, setFormValues] = useState<formValuesType>(initialValue);
-  const [formErrors, setFormErrors]: any = useState({});
-  const [displaySignInError, setDisplaySignInError] = useState(false);
-
-  const handleInputChange = (e: any) => {
-    const { name, value } = e.target;
-    setFormValues({ ...formValues, [name]: value });
-  };
-
-  const removeInputSpaces = (e: any) => {
-    const { name, value } = e.target;
-    setFormValues({ ...formValues, [name]: value.trim() });
-  };
-
-  const dispatch = useDispatch();
-
-  const handleSignIn = (e: any) => {
-    e.preventDefault();
-    if (Object.values(formValues).indexOf('') > -1) {
-      setFormErrors(validateSignInValues(formValues));
-    } else {
-      requestSignIn();
-    }
-  };
-
+export default function SignInForm() {
   const navigate = useNavigate();
-  const [requestLogIn] = useSignInMutation();
-
-  const requestSignIn = async () => {
-    requestLogIn(formValues);
-    console.log(formValues);
-    // try {
-    //   const response = await axios.post(
-    //     `${process.env.JENNAPARK_SERVER_URL}/api/v1/members/login`,
-    //     formValues,
-    //   );
-    //   setFormErrors(validateSignInValues(formValues));
-    //   setDisplaySignInError(false);
-    //   const { accessToken, name } = response.data;
-    //   window.localStorage.setItem('accessToken', accessToken);
-    //   dispatch(saveName(name));
-    //   navigate('/');
-    // } catch (e) {
-    //   setFormErrors(validateSignInValues(formValues));
-    //   setDisplaySignInError(true);
-    // }
-  };
-
-  const validateSignInValues = (values: any) => {
-    const errors: any = {};
-    if (!values.email) {
-      errors.email = '이메일을 입력해주세요!';
-    }
-    if (!values.password) {
-      errors.password = '비밀번호를 입력해주세요!';
-    }
-    return errors;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SignInFormInputs>();
+  const [requestSignIn] = useSignInMutation();
+  const onSubmit = (data: SignInFormInputs) => {
+    requestSignIn(data)
+      .unwrap()
+      .then(() => {
+        navigate('/');
+      });
   };
 
   return (
-    <form onSubmit={handleSignIn}>
-      <div className="input-wrapper">
-        <p className="label">이메일</p>
-        <input
-          autoFocus
-          type="text"
-          name="email"
-          placeholder="이메일을 입력해주세요"
-          value={formValues.email}
-          onChange={handleInputChange}
-          onBlur={removeInputSpaces}
-        />
-        <p
-          style={{ visibility: formErrors.email ? 'visible' : 'hidden' }}
-          className="error-message"
-        >
-          {formErrors.email}
-        </p>
-      </div>
-      <div className="input-wrapper">
-        <p className="label">비밀번호</p>
-        <input
-          type="password"
-          name="password"
-          placeholder="비밀번호를 입력해주세요"
-          value={formValues.password}
-          onChange={handleInputChange}
-          onBlur={removeInputSpaces}
-        />
-        <p
-          style={{ visibility: formErrors.password ? 'visible' : 'hidden' }}
-          className="error-message"
-        >
-          {formErrors.password}
-        </p>
-      </div>
-      <div className="button-wrapper">
-        <button
-          type="submit"
-          onClick={() => {
-            requestSignIn();
-          }}
-        >
-          로그인
-        </button>
-        <p
-          className="error-message"
-          style={{ visibility: displaySignInError ? 'visible' : 'hidden' }}
-        >
-          아이디가 존재하지 않거나 올바른 비밀번호가 아닙니다
-        </p>
-      </div>
-      <p className="signup-text">
-        아직 계정이 없으신가요?
-        <span onClick={() => navigate('/signup')}>회원가입</span>
-      </p>
-    </form>
-  );
-};
+    <S.Container>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <h1>서비스를 이용하기 위해 로그인을 진행해주세요.</h1>
+        <article>
+          <input {...register('email', { required: '이메일을 입력해 주세요' })} />
+          <label>이메일</label>
+          {errors.email && <p>{errors.email.message}</p>}
+        </article>
 
-export default SignInForm;
+        <article>
+          <input {...register('password', { required: '비밀번호를 입력해 주세요' })} />
+          <label>비밀번호</label>
+          {errors.password && <p>{errors.password.message}</p>}
+        </article>
+        <span onClick={() => navigate('/signup')}>회원가입</span>
+        <button type="submit">로그인</button>
+      </form>
+    </S.Container>
+  );
+}
