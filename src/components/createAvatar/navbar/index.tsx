9ca728e2c9ff457619304a127a81ef915bd74c92
ShinJaeIl01 <ekvm555@naver.com>
2022-10-02 20:10:53 +0900

    #41 💧fix: Working Navbar Button Function

--- src/components/createAvatar/navbar/index.tsx
+++ src/components/createAvatar/navbar/index.tsx
@@ -1,33 +1,19 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
+import React from 'react';
 import UserNavbarLayout from '../../../layout/NavigationBar/UserNavbarLayout';
 import WorkingNavbarLayout from '../../../layout/NavigationBar/WorkingNavbarLayout';
-import { useAppDispatch } from '../../../store/store';
+import { useAppSelector, useAppDispatch } from '../../../store/store';
 import { workingComponent } from '../../../store/workingProject/projectControlSlice';
 
 function Navbar() {
+  const { isVoiceWoking } = useAppSelector((state) => state.projectControl.elementData);
   const dispatch = useAppDispatch();
-  const [voiceSection, setVoiceSection] = useState(true);
-  const [avatarSection, setAvatarSection] = useState(false);
-  function voiceHandler() {
+  function buttonHandler() {
     dispatch(workingComponent());
-    setVoiceSection(true);
-    setAvatarSection(false);
-  }
-  function avatarHandler() {
-    dispatch(workingComponent());
-    setAvatarSection(true);
-    setVoiceSection(false);
   }
   return (
     <>
       <UserNavbarLayout />
-      <WorkingNavbarLayout
-        voiceButton={voiceHandler}
-        avatarButton={avatarHandler}
-        voiceBg={voiceSection}
-        avatarBg={avatarSection}
-      />
+      <WorkingNavbarLayout buttonHandler={buttonHandler} iconBg={isVoiceWoking} />
     </>
   );
 }
