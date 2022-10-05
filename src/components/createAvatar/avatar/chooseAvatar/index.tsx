d672ebc9668bf7c73fdb62e9dca15538eef6bcb2
Kimchaewook <allurings2@naver.com>
2022-10-05 19:44:23 +0900

    #12 ✨ feat: CreateAvatar Api Data Passing to avatarSlice

--- src/components/createAvatar/avatar/chooseAvatar/index.tsx
+++ src/components/createAvatar/avatar/chooseAvatar/index.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import AvatarChooseLayout from '../../../../layout/avatar/AvatarChooseLayout';
 import { useGetAvatarChooseListQuery, useGetAvatarChooseListIdQuery, usePostCreateAvatarMutation } from '../../../../api/useApi';
 import { useAppSelector, useAppDispatch } from '../../../../store/store';
-import { avatarModelKindSelect, avatarModelKindReset } from '@/store/Avatar/avatarSlice';
+import { avatarModelKindSelect, avatarModelKindReset, avatarChooseDataUrl } from '@/store/Avatar/avatarSlice';
 
 function index() {
 
@@ -11,6 +11,7 @@ function index() {
 
   // 아바타 의상 선택
   function chooseAvatarModelKind(id:number, kind:string) {
+    console.log(avartarDress)
     dispatch(avatarModelKindSelect({id, kind}));
   }
 
@@ -20,7 +21,7 @@ function index() {
   }
 
   // 아바타 불러오기
-  let [avatarId, setAvartarId] = useState<any>(0);
+  let [avatarId, setAvartarId] = useState<any> (0);
   const { data: avatarList } = useGetAvatarChooseListQuery(null)
 
   // 아바타 선택
@@ -28,14 +29,12 @@ function index() {
   useEffect(() => {}, [avatarListDress])
 
   // 아바타 만들기
+  const [createAvatar, {data: avatarUrlData}] = usePostCreateAvatarMutation();
+  function createAvatarHandler() {
+    createAvatar(avartarDress)
+    dispatch(avatarChooseDataUrl(avatarUrlData!.data))
+  }
 
-  const [createAvatar] = usePostCreateAvatarMutation();
-
-  // function createAvatars() {
-  //   const [createAvatar] = usePostCreateAvatarMutation(avartarDress);
-  // }
-
-  
   return (
     <>
       <AvatarChooseLayout
@@ -45,27 +44,10 @@ function index() {
       avatarListDress={avatarListDress}
       avatarModelSelect={chooseAvatarModelKind}
       avatarModelReset={resetAvatarModelKind}
-      createAvatar = {createAvatar}
-      avartarDress = {avartarDress}
+      createAvatarHandler={createAvatarHandler}
       />
     </>
   )
-}
-
-// interface Test {
-//   avatarList: {
-//     id: number;
-//     name: string;
-//     thumbNail: string;
-//   }[]
-// }
-
-interface avatarDressType {
-    accessoryld: number;
-    avatarld: number;
-    clothesId: number;
-    hatId: number;
-    projectId: number;
 }
 
 export default index
