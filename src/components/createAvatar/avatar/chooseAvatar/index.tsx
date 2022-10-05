ce61773b4e532cf0b699b05f92b2e3ec6feab83f
Kimchaewook <allurings2@naver.com>
2022-10-05 14:42:54 +0900

    #12 ✨feat: Add api connect and props pnasing

--- src/components/createAvatar/avatar/chooseAvatar/index.tsx
+++ src/components/createAvatar/avatar/chooseAvatar/index.tsx
@@ -1,20 +1,71 @@
-import { useState } from 'react';
-import AvatarComponent from '../../../../styles/AvatarComponent';
-import AvatarChooseStyle from '../../../../styles/AvatarChooseStyle';
+import { useState, useEffect } from 'react';
+import AvatarChooseLayout from '../../../../layout/avatar/AvatarChooseLayout';
+import { useGetAvatarChooseListQuery, useGetAvatarChooseListIdQuery, usePostCreateAvatarMutation } from '../../../../api/useApi';
+import { useAppSelector, useAppDispatch } from '../../../../store/store';
+import { avatarModelKindSelect, avatarModelKindReset } from '@/store/Avatar/avatarSlice';
 
 function index() {
 
-  const [toggle, setToggle] = useState(true);
+  const avartarDress = useAppSelector((state) => state.avatar.avatarModel)
+  const dispatch = useAppDispatch()
 
-  function toggleButton() {
-    setToggle(!toggle);
+  // 아바타 의상 선택
+  function chooseAvatarModelKind(id:number, kind:string) {
+    dispatch(avatarModelKindSelect({id, kind}));
   }
 
+  // 아바타 의상 선택 초기화
+  function resetAvatarModelKind(id:number) {
+    dispatch(avatarModelKindReset(id));
+  }
+
+  // 아바타 불러오기
+  let [avatarId, setAvartarId] = useState(0);
+  const { data: avatarList } = useGetAvatarChooseListQuery(null)
+
+  // 아바타 선택
+  const { data: avatarListDress } = useGetAvatarChooseListIdQuery(avatarId)
+  useEffect(() => {}, [avatarListDress])
+
+  // 아바타 만들기
+
+  const [createAvatar] = usePostCreateAvatarMutation();
+
+  // function createAvatars() {
+  //   const [createAvatar] = usePostCreateAvatarMutation(avartarDress);
+  // }
+
+  
   return (
     <>
-      <AvatarChooseStyle></AvatarChooseStyle>
+      <AvatarChooseLayout
+      avatarList={avatarList}
+      setAvatarId={setAvartarId}
+      avatarId={avatarId}
+      avatarListDress={avatarListDress}
+      avatarModelSelect={chooseAvatarModelKind}
+      avatarModelReset={resetAvatarModelKind}
+      createAvatar = {createAvatar}
+      avartarDress = {avartarDress}
+      />
     </>
   )
+}
+
+// interface Test {
+//   avatarList: {
+//     id: number;
+//     name: string;
+//     thumbNail: string;
+//   }[]
+// }
+
+interface avatarDressType {
+    accessoryld: number;
+    avatarld: number;
+    clothesId: number;
+    hatId: number;
+    projectId: number;
 }
 
 export default index
