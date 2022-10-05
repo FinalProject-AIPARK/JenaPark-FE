67f4ef18b41508175fe75624d63738ebd4bb397d
Suhyxn <momentomori21489@gmail.com>
2022-10-05 19:00:48 +0900

    ✨ feat : Add A Landing Page Video-#3

--- src/layout/LandingPageStyle.tsx
+++ src/layout/LandingPageStyle.tsx
@@ -1,7 +1,30 @@
 import React from 'react';
+import YouTube from 'react-youtube';
 
 function LandingPageStyle() {
-  return <div>LandingPageStyle</div>;
+  return (
+    <>
+      <YouTube
+        videoId="hUgGblPDVxI"
+        opts={{
+          width: '600',
+          height: '374',
+          playerVars: {
+            autoplay: 1,
+            rel: 0,
+            modestbranding: 1,
+            loop: 1,
+            playlist: 'hUgGblPDVxI',
+            controls: 0,
+            end: 20,
+            fs: 1,
+            disablekb: 1,
+            mute: 1,
+          },
+        }}
+      />
+    </>
+  );
 }
 
 export default LandingPageStyle;
