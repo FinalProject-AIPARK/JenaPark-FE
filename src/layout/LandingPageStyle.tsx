e56ad1cf9399175ba470ad234bb01a48c755a02f
Suhyxn <momentomori21489@gmail.com>
2022-10-05 21:30:05 +0900

    💇style : Add Title And Subtitle Text-#3

--- src/layout/LandingPageStyle.tsx
+++ src/layout/LandingPageStyle.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import styled from 'styled-components';
 import YouTube from 'react-youtube';
 
 function LandingPageStyle() {
   return (
     <>
+      <Title>본 서비스로 시공간을 초월하세요</Title>
+      <SubTitle>
+        본 서비스는 인공지능과 영상 기술의 융햡을 통해 <br />
+        영상 안에서 시간과 공간, 능력을 초월하기 위한 서비스입니다
+      </SubTitle>
       <YouTube
         videoId="hUgGblPDVxI"
         opts={{
@@ -26,5 +32,19 @@ function LandingPageStyle() {
     </>
   );
 }
+
+const Title = styled.div`
+  color: #fff;
+  font-size: 3.75rem;
+  margin-top: 8%;
+  line-height: 5.4375rem;
+`;
+
+const SubTitle = styled.div`
+  color: #fff;
+  font-size: 1.875rem;
+  margin-top: 1rem;
+  line-height: 2.6875rem;
+`;
 
 export default LandingPageStyle;
