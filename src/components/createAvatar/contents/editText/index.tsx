84195c2be01153d9348cff5ba6ebf1f404bcfbd6
Kimchaewook <allurings2@naver.com>
2022-10-12 01:17:24 +0900

    #29 ✨ feat: Add audioText

--- src/components/createAvatar/contents/editText/index.tsx
+++ src/components/createAvatar/contents/editText/index.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import EditTextLayout from '../../../../layout/editText/EditTextLayout'
+import { useAppSelector, useAppDispatch } from '../../../../store/store';
 
 function index() {
+
+  const audioText = useAppSelector((state) => state.voice.inputTextSynth)
+  console.log(audioText)
+
   return (
     <div>
-      <EditTextLayout/>
+      <EditTextLayout
+      audioText={audioText}
+      />
     </div>
   )
 }
