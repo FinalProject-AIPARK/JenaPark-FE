1005289513389f221c495d76cfe74cea35a38aea
Suhyxn <momentomori21489@gmail.com>
2022-10-11 15:12:11 +0900

    ✨ feat : Add Link Function At The Bottom Right-#51

--- src/layout/LendingFooter.tsx
+++ src/layout/LendingFooter.tsx
@@ -17,9 +17,16 @@ function LendingFooter() {
         </ContentContainer>
 
         <LinkContainer>
+          <LinkImage onClick={() => window.open('https://www.aipark.co.kr/', '_blank')} />
           <LinkImage />
-          <LinkImage />
-          <LinkImage />
+          <LinkImage
+            onClick={() =>
+              window.open(
+                'https://www.youtube.com/c/%EC%97%90%EC%9D%B4%EC%95%84%EC%9D%B4%ED%8C%8C%ED%81%ACAIPARK',
+                '_blank',
+              )
+            }
+          />
         </LinkContainer>
       </FooterContainer>
     </>
@@ -67,6 +74,7 @@ const LinkImage = styled.img.attrs({
   height: 1.125rem;
   margin-right: 1.875rem;
   padding-top: 0.5rem;
+  cursor: pointer;
 
   &:last-child {
     margin-right: 0.2;
