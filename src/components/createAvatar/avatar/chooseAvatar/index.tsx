a5c6f2042223e6733a1c6546fb1b806c1e638712
Kimchaewook <allurings2@naver.com>
2022-10-12 14:44:11 +0900

    #12 ✨ feat: Add Slide and Store in ProjectId

--- src/components/createAvatar/avatar/chooseAvatar/index.tsx
+++ src/components/createAvatar/avatar/chooseAvatar/index.tsx
@@ -1,53 +1,94 @@
-import { useState, useEffect } from 'react';
-import AvatarChooseLayout from '../../../../layout/avatar/AvatarChooseLayout';
-import { useGetAvatarChooseListQuery, useGetAvatarChooseListIdQuery, usePostCreateAvatarMutation } from '../../../../api/useApi';
-import { useAppSelector, useAppDispatch } from '../../../../store/store';
-import { avatarModelKindSelect, avatarModelKindReset, avatarChooseDataUrl } from '@/store/Avatar/avatarSlice';
+import { useState, useEffect } from "react";
+import AvatarChooseLayout from "../../../../layout/avatar/AvatarChooseLayout";
+import {
+  useGetAvatarChooseListQuery,
+  useGetAvatarChooseListIdQuery,
+  usePostCreateAvatarMutation,
+} from "../../../../api/useApi";
+import { useAppSelector, useAppDispatch } from "../../../../store/store";
+import {
+  avatarModelKindSelect,
+  avatarModelKindReset,
+  avatarChooseDataUrl,
+  avatarProjectId
+} from "@/store/Avatar/avatarSlice";
 
 function index() {
-
-  const avartarDress = useAppSelector((state) => state.avatar.avatarModel)
-  const dispatch = useAppDispatch()
+  const avartarDress = useAppSelector((state) => state.avatar.avatarModel);
+  const { projectId } = useAppSelector(
+    (state) => state.projectControl.projectData
+  );
+  const dispatch = useAppDispatch();
 
   // 아바타 의상 선택
-  function chooseAvatarModelKind(id:number, kind:string) {
-    console.log(avartarDress)
-    dispatch(avatarModelKindSelect({id, kind}));
+  function chooseAvatarModelKind(id: number, kind: string) {
+    dispatch(avatarModelKindSelect({ id, kind }));
   }
 
   // 아바타 의상 선택 초기화
-  function resetAvatarModelKind(id:number) {
+  function resetAvatarModelKind(id: number) {
     dispatch(avatarModelKindReset(id));
   }
 
   // 아바타 불러오기
-  let [avatarId, setAvartarId] = useState<any> (0);
-  const { data: avatarList } = useGetAvatarChooseListQuery(null)
+  let [avatarId, setAvartarId] = useState<any>(0);
+  const { data: avatarList } = useGetAvatarChooseListQuery(null);
 
   // 아바타 선택
-  const { data: avatarListDress } = useGetAvatarChooseListIdQuery(avatarId)
-  useEffect(() => {}, [avatarListDress])
+  const { data: avatarListDress } = useGetAvatarChooseListIdQuery(avatarId);
+  useEffect(() => {}, [avatarListDress]);
 
   // 아바타 만들기
-  const [createAvatar, {data: avatarUrlData}] = usePostCreateAvatarMutation();
+  const [createAvatar, { data: avatarUrlData }] = usePostCreateAvatarMutation();
   function createAvatarHandler() {
-    createAvatar(avartarDress)
-    dispatch(avatarChooseDataUrl(avatarUrlData!.data))
+    createAvatar(avartarDress);
   }
+
+  // 슬라이드 기능
+  let [avatarSlideIndex, setAvatarSlideIndex] = useState({
+    avatar: 0,
+    acc: 0,
+    attitude: 0,
+    clothes: 0,
+  });
+
+  function moveAvatarSlide(leftRight: string, maxLength: number, kind: string) {
+    if (avatarSlideIndex[kind] === 0 && leftRight === "left") return;
+    else if (avatarSlideIndex[kind] === maxLength * -1 && leftRight === "right")
+      return;
+    else if (leftRight === "right") {
+      const copy = { ...avatarSlideIndex };
+      copy[kind] -= 1;
+      setAvatarSlideIndex(copy);
+    } else if (leftRight === "left") {
+      const copy = { ...avatarSlideIndex };
+      copy[kind] += 1;
+      setAvatarSlideIndex(copy);
+    }
+  }
+
+  useEffect(() => {
+    avatarUrlData !== undefined
+      ? dispatch(avatarChooseDataUrl(avatarUrlData!.data))
+      : null;
+  }, [avatarUrlData]);
+  dispatch(avatarProjectId(projectId));
 
   return (
     <>
       <AvatarChooseLayout
-      avatarList={avatarList}
-      setAvatarId={setAvartarId}
-      avatarId={avatarId}
-      avatarListDress={avatarListDress}
-      avatarModelSelect={chooseAvatarModelKind}
-      avatarModelReset={resetAvatarModelKind}
-      createAvatarHandler={createAvatarHandler}
+        avatarList={avatarList}
+        setAvatarId={setAvartarId}
+        avatarId={avatarId}
+        avatarListDress={avatarListDress}
+        avatarModelSelect={chooseAvatarModelKind}
+        avatarModelReset={resetAvatarModelKind}
+        createAvatarHandler={createAvatarHandler}
+        avatarSlideIndex={avatarSlideIndex}
+        moveAvatarSlide={moveAvatarSlide}
       />
     </>
-  )
+  );
 }
 
-export default index
+export default index;
