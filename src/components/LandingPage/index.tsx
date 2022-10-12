9663fc9ca9409d5a6a938340c7fc32ea5d0203e2
Kim Mingu <MINGU757377@gmail.com>
2022-10-12 16:04:46 +0900

    #1 ⚙️ conf: Update Google login

--- src/components/LandingPage/index.tsx
+++ src/components/LandingPage/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import LandingPageStyle from '@/layout/LandingPageStyle';
 import Header from '../Header';
 import Footer from '@/layout/LendingFooter';
@@ -8,6 +8,16 @@ import { Cookies } from 'react-cookie';
 import LandingFooter from '../Footer/LandingFooter';
 
 function LandingPage() {
+  const cookies = new Cookies();
+  const url = new URL(window.location.href);
+  const urlParams = url.searchParams;
+  useEffect(() => {
+    if (urlParams) {
+      cookies.set('accessToken', urlParams.get('accessToken'));
+      cookies.set('refreshToken', urlParams.get('refreshToken'));
+    }
+  }, [urlParams]);
+  console.log(urlParams.get('accessToken'));
   return (
     <>
       <Header />
