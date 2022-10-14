761f01d75d84ad51ba6a46fd4f282a1ee964512b
January <101620064+JaeIL00@users.noreply.github.com>
2022-10-14 12:52:51 +0900

    Delete src/components/Header directory

--- src/components/Header/SoundPlayer/index.tsx
+++
@@ -1,9 +0,0 @@
-import React, { useRef } from 'react';
-import SoundPlayerLayout from '@/layout/SoundPlayer';
-import 'react-h5-audio-player/lib/styles.css';
-
-function SoundPlayer() {
-  return <SoundPlayerLayout />;
-}
-
-export default SoundPlayer;
